8d143a2f78e1fa6c403934437dc31413ad2e9b14
wrnlb <103810793+wrnlb666@users.noreply.github.com>
2023-03-01 03:48:24 -0500

    basically rewrote the entire library

--- str.c
+++ str.c
@@ -10,68 +10,72 @@
 
 
 
-static inline bool str_resize( string_t* string, size_t size )
+static inline bool str_resize( string_t** string, size_t size )
 {
     size_t cap;
-    string->length = size;
+    if ( *string == NULL )
+    {
+        *string = malloc( sizeof ( string_t ) );
+        (*string)->capacity = 16;
+    }
+    (*string)->length = size;
     if ( size < 16 ) cap = 16;
     else cap = size + 1;
-    if ( cap > string->capacity )
+    if ( cap > (*string)->capacity )
     {
-        while ( string->capacity < cap )
+        while ( (*string)->capacity < cap )
         {
-            if ( string->capacity <= 1024 )
+            if ( (*string)->capacity <= 1024 )
             {
-                string->capacity *= 2;
+                (*string)->capacity *= 2;
             }
             else
             {
-                string->capacity += 512;
+                (*string)->capacity += 512;
             }
         }
-        string->cstr = realloc( string->cstr, sizeof ( char ) * string->capacity );
-        if ( string->cstr == NULL ) return ( fputs( "[ERRO]: run out of memory", stderr ), false );
+        (*string) = realloc( (*string), sizeof ( string_t ) + sizeof ( char ) * ( (*string)->capacity - 1 ) );
+        if ( (*string) == NULL ) return ( fputs( "[ERRO]: run out of memory", stderr ), false );
     }
     else
     {
         while ( cap > 1024 )
         {
-            if ( cap + 512 < string->capacity )
+            if ( cap + 512 < (*string)->capacity )
             {
-                string->capacity -= 512;
+                (*string)->capacity -= 512;
             }
             else break;
         }
         while ( cap <= 1024 )
         {
-            if ( cap * 2 < string->capacity )
+            if ( cap * 2 < (*string)->capacity )
             {
-                string->capacity /= 2;
+                (*string)->capacity /= 2;
             }
             else break;
         }
-        string->cstr = realloc( string->cstr, sizeof ( char ) * string->capacity );
-        if ( string->cstr == NULL ) return ( fputs( "[ERRO]: run out of memory", stderr ), false );
+        *string = realloc( *string, sizeof ( string_t ) + sizeof ( char ) * ( (*string)->capacity - 1 ) );
+        if ( (*string) == NULL ) return ( fputs( "[ERRO]: run out of memory", stderr ), false );
     }
-    string->cstr[ string->length ] = 0;
+    (*string)->cstr[ (*string)->length ] = 0;
     return true;
 }
 
 
-string_t str_new_string( char* src )
+string_t* str_new_string( char* src )
 {
-    string_t string = { .length = strlen(src), .capacity = 16 };
-    if ( str_resize( &string, string.length ) )
+    string_t* string = NULL;
+    if ( str_resize( &string, strlen(src) ) )
     {
-        memmove( string.cstr, src, string.length );
-        string.cstr[ string.length ] = 0;
+        strcpy( string->cstr, src );
         return string;
     }
-    return (string_t) { 0 };
+    return NULL;
 }
 
 
-string_t str_new_strings( char* src, ... )
+string_t* str_new_strings( char* src, ... )
 {
     va_list ap, _ap;
     va_start( ap, src );
@@ -83,23 +87,23 @@ string_t str_new_strings( char* src, ... )
     }
     va_end(ap);
     size_t index = 0;
-    string_t result = { .length = length, .capacity = 16 };
+    string_t* result = NULL;
     if ( str_resize( &result, length ) )
     {
         for ( char* str = src; str!= NULL; str = va_arg( _ap, char* ) )
         {
-            strcpy( result.cstr + index, str );
+            strcpy( result->cstr + index, str );
             index += strlen( str );
         }
         va_end(_ap);
         return result;
     }
     va_end(_ap);
-    return (string_t) { 0 };
+    return NULL;
 }
 
 
-string_t str_new_string_arr( char** src )
+string_t* str_new_string_arr( char** src )
 {
     size_t length = 0;
     for ( size_t i = 0; src[i] != NULL; i++ )
@@ -107,26 +111,23 @@ string_t str_new_string_arr( char** src )
         length += strlen(src[i]);
     }
     size_t index = 0;
-    string_t result = { .length = length, .capacity = 16 };
+    string_t* result = NULL;
     if ( str_resize( &result, length ) )
     {
         for ( size_t i = 0; src[i] != NULL; i++ )
         {
-            strcpy( result.cstr + index, src[i] );
+            strcpy( result->cstr + index, src[i] );
             index += strlen( src[i] );
         }
         return result;
     }
-    return (string_t) { 0 };
+    return NULL;
 }
 
 
 void str_destroy_string( string_t* string )
 {
-    free( string->cstr );
-    string->capacity = 0;
-    string->length = 0;
-    string->cstr = NULL;
+    free( string );
 }
 
 
@@ -136,18 +137,17 @@ void str_destroy_strings( string_t* string, ... )
     va_start( ap, string );
     for ( string_t* str = string; str != NULL; str = va_arg( ap, string_t* ) )
     {
-        str_destroy_string( str );
+        free( str );
     }
     va_end( ap );
 }
 
 
-void str_destroy_string_arr( string_t* str_arr )
+void str_destroy_string_arr( string_t** str_arr )
 {
-    int index = 0;
-    while ( str_arr[index].capacity )
+    for ( size_t i = 0; str_arr[i] != NULL; i++ )
     {
-        str_destroy_string( &str_arr[index++] );
+        free( str_arr[i] );
     }
     free( str_arr );
 }
@@ -155,7 +155,7 @@ void str_destroy_string_arr( string_t* str_arr )
 
 bool str_clear( string_t* string )
 {
-    if ( str_resize( string, 0 ) )
+    if ( str_resize( &string, 0 ) )
     {
         memset( string->cstr, 0, sizeof (char) * 16 );
         return true;
@@ -164,101 +164,101 @@ bool str_clear( string_t* string )
 }
 
 
-string_t str_append( string_t start, string_t end )
+string_t* str_append( const string_t* start, const string_t* end )
 {
-    size_t length = start.length + end.length;
-    string_t result = { .capacity = 16  };
+    size_t length = start->length + end->length;
+    string_t* result = NULL;
     if ( str_resize( &result, length ) )
     {
-        memmove( result.cstr, start.cstr, start.length );
-        memmove( result.cstr + start.length, end.cstr, end.length );
-        result.cstr[ result.length ] = 0;
+        memmove( result->cstr, start->cstr, start->length );
+        memmove( result->cstr + start->length, end->cstr, end->length );
+        result->cstr[ result->length ] = 0;
         return result;
     }
-    return (string_t) { 0 };
+    return NULL;
 }
 
 
-string_t str_appends( string_t start, ... )
+string_t* str_appends( const string_t* start, ... )
 {
     va_list ap, _ap;
     va_start( ap, start );
     va_copy( _ap, ap );
     size_t length;
-    for ( string_t str = start; str.cstr != NULL; str = va_arg( ap, string_t ) )
+    for ( const string_t* str = start; str != NULL; str = va_arg( ap, string_t* ) )
     {
-        length += start.length;
+        length += start->length;
     }
     va_end(ap);
-    string_t result = { .length = length, .capacity = 16 };
+    string_t* result = NULL;
     if ( str_resize( &result, length ) )
     {
         size_t index = 0;
-        for ( string_t str = start; str.cstr != NULL; str = va_arg( _ap, string_t ) )
+        for ( const string_t* str = start; str != NULL; str = va_arg( _ap, string_t* ) )
         {
-            strcpy( result.cstr + index, str.cstr );
-            index += str.length;
+            strcpy( result->cstr + index, str->cstr );
+            index += str->length;
         }
         va_end(_ap);
         return result;
     }
     va_end(_ap);
-    return (string_t) { 0 };
+    return NULL;
 }
 
 
-string_t str_append_cstr( string_t start, const char* end )
+string_t* str_append_cstr( const string_t* start, const char* end )
 {
-    size_t start_len = start.length;
+    size_t start_len = start->length;
     size_t end_len = strlen(end);
-    string_t result = { .capacity = 16 };
+    string_t* result = NULL;
     if ( str_resize( &result, start_len + end_len ) )
     {
-        memmove( result.cstr, start.cstr, start_len );
-        memmove( result.cstr, end, end_len );
-        result.cstr[ result.length ] = 0;
+        memmove( result->cstr, start->cstr, start_len );
+        memmove( result->cstr, end, end_len );
+        result->cstr[ result->length ] = 0;
         return result;
     }
-    return (string_t) { 0 };
+    return NULL;
 }
 
 
-string_t str_append_cstrs( string_t start, ... )
+string_t* str_append_cstrs( const string_t* start, ... )
 {
     va_list ap, _ap;
     va_start( ap, start );
     va_copy( _ap, ap );
     size_t length = 0;
-    for ( char* string = start.cstr; string != NULL; string = va_arg( ap, char* ) )
+    for ( const char* string = start->cstr; string != NULL; string = va_arg( ap, char* ) )
     {
         length += strlen( string );
     }
     va_end(ap);
-    string_t result = { .length = length, .capacity = 16 };
+    string_t* result = NULL;
     if ( str_resize( &result, length ) )
     {
         size_t index = 0;
-        for ( char* string = start.cstr; string != NULL; string = va_arg( _ap, char* ) )
+        for ( const char* string = start->cstr; string != NULL; string = va_arg( _ap, char* ) )
         {
-            strcpy( result.cstr + index, string );
+            strcpy( result->cstr + index, string );
             index += strlen( string );
         }
         va_end( _ap );
         return result;
     }
     va_end( _ap );
-    return (string_t) { 0 };
+    return NULL;
 }
 
 
-string_t* str_split( string_t src, const char* needle )
+string_t** str_split( const string_t* src, const char* needle )
 {
     size_t nlen = strlen( needle );
-    char* str = malloc( sizeof (char) * ( src.length + 1 ) );
-    strncpy( str, src.cstr, src.length + 1 );
+    char* str = malloc( sizeof (char) * ( src->length + 1 ) );
+    strncpy( str, src->cstr, src->length + 1 );
     char* ptr = str;
     size_t cap = 16;
-    string_t* tokens = malloc( sizeof ( string_t ) * cap );
+    string_t** tokens = malloc( sizeof ( string_t* ) * cap );
     char* token;
     size_t index = 0;
 
@@ -266,23 +266,23 @@ string_t* str_split( string_t src, const char* needle )
     while ( token != NULL )
     {
         *token = 0;
-        tokens[index] = (string_t) { .length = strlen(str), .capacity = 16 };
-        if ( !str_resize( &tokens[index], tokens[index].length ) ) return ( free(ptr), NULL );
-        strcpy( tokens[index].cstr, str );
+        tokens[index] = NULL;
+        if ( !str_resize( &tokens[index], strlen(str) ) ) return ( free(ptr), NULL );
+        strcpy( tokens[index]->cstr, str );
         str = token + nlen;
         index++;
         token = strstr( str, needle );
         if ( index + 2 == cap )
         {
             cap += 16;
-            tokens = realloc( tokens, sizeof ( string_t ) * cap );
+            tokens = realloc( tokens, sizeof ( string_t* ) * cap );
         }
     }
-    tokens[index] = (string_t) { .length = strlen(str), .capacity = 16 };
-    if ( !str_resize( &tokens[index], tokens[index].length ) ) return ( free(ptr), NULL );
-    strcpy( tokens[index].cstr, str );
+    tokens[index] = NULL;
+    if ( !str_resize( &tokens[index], strlen(str) ) ) return ( free(ptr), NULL );
+    strcpy( tokens[index]->cstr, str );
     index++;
-    tokens[index] = (string_t) { 0 };
+    tokens[index] = NULL;
     free(ptr);
     return tokens;
 }
@@ -306,26 +306,26 @@ void str_to_lower( string_t* string )
 }
 
 
-string_t str_substr( string_t src, size_t start, size_t size )
+string_t* str_substr( const string_t* src, size_t start, size_t size )
 {
-    if ( start + size > src.length )
+    if ( start + size > src->length )
     {
-        return ( fputs( "[ERRO]: substring out of bound!", stderr ), (string_t) { 0 } );
+        return ( fputs( "[ERRO]: substring out of bound!", stderr ), NULL );
     }
-    string_t substr = { .length = size, .capacity = 16 };
+    string_t* substr = NULL;
     str_resize( &substr, size );
-    strncpy( substr.cstr, src.cstr + start, size );
-    substr.cstr[ substr.length ] = 0;
+    strncpy( substr->cstr, src->cstr + start, size );
+    substr->cstr[ substr->length ] = 0;
     return substr;
 }
 
 
-string_t str_replace( string_t src, const char* old_val, const char* new_val )
+string_t* str_replace( const string_t* src, const char* old_val, const char* new_val )
 {
     // creating string arrays
     size_t old_len = strlen( old_val );
-    char* str = malloc( sizeof (char) * ( src.length + 1 ) );
-    strncpy( str, src.cstr, src.length + 1 );
+    char* str = malloc( sizeof (char) * ( src->length + 1 ) );
+    strncpy( str, src->cstr, src->length + 1 );
     char* ptr = str;
     size_t cap = 16;
     char** tokens = malloc( sizeof (char*) * cap );
@@ -363,16 +363,16 @@ string_t str_replace( string_t src, const char* old_val, const char* new_val )
         length += new_len;
     }
     index = 0;
-    string_t result = { .length = length, .capacity = 16 };
+    string_t* result = NULL;
     if ( str_resize( &result, length ) )
     {
         for ( size_t i = 0; tokens[i] != NULL; i++ )
         {
-            strcpy( result.cstr + index, tokens[i] );
+            strcpy( result->cstr + index, tokens[i] );
             index += strlen( tokens[i] );
             if ( counter != i )
             {
-                strcpy( result.cstr + index, new_val );
+                strcpy( result->cstr + index, new_val );
                 index += new_len;
             }
             free( tokens[i] );
@@ -385,5 +385,5 @@ string_t str_replace( string_t src, const char* old_val, const char* new_val )
         free( tokens[i] );
     }
     free( tokens );
-    return (string_t) { 0 };
+    return NULL;
 }
--- str.h
+++ str.h
@@ -17,18 +17,18 @@ typedef struct string_t
 {
     size_t length;
     size_t capacity;
-    char* cstr;
+    char cstr[1];
 } string_t;
 
 
 // constructor
-string_t str_new_string( char* src );
+string_t* str_new_string( char* src );
 
 // constructor that take in multiple null terminated strings at once, the last argument has to be NULL
-string_t str_new_strings( char* src, ... );
+string_t* str_new_strings( char* src, ... );
 
 // constructor that take in an array of c strings, the last element of the array has to be NULL
-string_t str_new_string_arr( char** src );
+string_t* str_new_string_arr( char** src );
 
 // free memory allocated by string internally
 void str_destroy_string( string_t* string );
@@ -39,27 +39,27 @@ void str_destroy_strings( string_t* string, ... );
 // free an array of string, the last element of the array has to be (string_t) { 0 }
 // the array must be allocated on the heap since this function also calls free on the array. 
 // this function is intended to free the array returned by this library
-void str_destroy_string_arr( string_t* str_arr );
+void str_destroy_string_arr( string_t** str_arr );
 
 // effectivly clear the string, set the length to 0, may or may not change the capacity. 
 bool str_clear( string_t* string );
 
 // append two strings together, address can be overlapped
-string_t str_append( string_t start, string_t end );
+string_t* str_append( const string_t* start, const string_t* end );
 
 // append strings together, the last element has to be (string_t) { 0 }
-string_t str_appends( string_t start, ... );
+string_t* str_appends( const string_t* start, ... );
 
 // add null terminated c type string to string_t, return a string_t
-string_t str_append_cstr( string_t start, const char* end );
+string_t* str_append_cstr( const string_t* start, const char* end );
 
 // add null terminated c type strings to string_t, the last element has to be NULL, return a string_t
-string_t str_append_cstrs( string_t start, ... );
+string_t* str_append_cstrs( const string_t* start, ... );
 
 // split src string upon needle string, returns an array of string_t with the last element being (string_t) { 0 }
 // the easiest way to check if the array ends is to check if `string_t.cstr == NULL`
 // caller free, call `void str_destroy_string_arr( string_t* str_arr )` first then call `free` on the returned pointer
-string_t* str_split( string_t src, const char* needle );
+string_t** str_split( const string_t* src, const char* needle );
 
 // set all the characters to upper case, UB if special characters
 void str_to_upper( string_t* string );
@@ -68,10 +68,10 @@ void str_to_upper( string_t* string );
 void str_to_lower( string_t* string );
 
 // return a substr starting from index start, with size size
-string_t str_substr( string_t src, size_t start, size_t size );
+string_t* str_substr( const string_t* src, size_t start, size_t size );
 
 // replace old with new, returning a new string_t, src is not changed
-string_t str_replace( string_t src, const char* old, const char* new );
+string_t* str_replace( const string_t* src, const char* old_val, const char* new_val );
 
 
 
